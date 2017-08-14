e06bf230f611c8306fda96e32e3444d04fce3a2a
a1504 <a150410306947@gmail.com>
2017-08-14 11:41:25 -0500

    fc

---
+++ src/main/java/com/tedrum/web/HelloAppEngine.java
@@ -0,0 +1,18 @@
+package com.tedrum.web;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloAppEngine extends HttpServlet {
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+      
+    response.setContentType("text/plain");
+    response.getWriter().print("Hello App Engine with Tedrum!\r\n");
+
+  }
+}
